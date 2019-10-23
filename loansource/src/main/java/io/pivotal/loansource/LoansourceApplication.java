2b40534d43d8e2583e8ad7994b3ad88aec21ac31
sibendu_das <sibendu.das@oracle.com>
2019-10-24 00:07:46 +0530

    .

--- loansource/src/main/java/io/pivotal/loansource/LoansourceApplication.java
+++ loansource/src/main/java/io/pivotal/loansource/LoansourceApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,25 +14,28 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 @SpringBootApplication
+@EnableScheduling
 public class LoansourceApplication {
 
-  private static final Logger log = LoggerFactory.getLogger(LoansourceApplication.class);
-  private List<String> names = Arrays.asList("Donald", "Theresa", "Vladimir", "Angela", "Emmanuel", "Shinzō", "Jacinda", "Kim");
-  private List<Long> amounts = Arrays.asList(10L, 100L, 1000L, 10000L, 100000L, 1000000L, 10000000L, 100000000L, 100000000L);
+	private static final Logger log = LoggerFactory.getLogger(LoansourceApplication.class);
+	private List<String> names = Arrays.asList("Donald", "Theresa", "Vladimir", "Angela", "Emmanuel", "ShinzÅ�",
+			"Jacinda", "Kim");
+	private List<Long> amounts = Arrays.asList(10L, 100L, 1000L, 10000L, 100000L, 1000000L, 10000000L, 100000000L,
+			100000000L);
 
-  public static void main(String[] args) {
-    SpringApplication.run(LoansourceApplication.class, args);
-    log.info("The Loansource Application has started...");
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(LoansourceApplication.class, args);
+		log.info("The Loansource Application has started...");
+	}
 
-  @Bean
-  public Supplier<Loan> supplyLoan() {
-    return () -> {
-      String rName = names.get(new Random().nextInt(names.size()));
-      Long rAmount = amounts.get(new Random().nextInt(amounts.size()));
-      Loan loan = new Loan(UUID.randomUUID().toString(), rName, rAmount);
-      log.info("{} {} for ${} for {}", loan.getStatus(), loan.getUuid(), loan.getAmount(), loan.getName());
-      return loan;
-    };
-  }
+	@Bean
+	public Supplier<Loan> supplyLoan() {
+		return () -> {
+			String rName = names.get(new Random().nextInt(names.size()));
+			Long rAmount = amounts.get(new Random().nextInt(amounts.size()));
+			Loan loan = new Loan(UUID.randomUUID().toString(), rName, rAmount);
+			log.info("{} {} for ${} for {}", loan.getStatus(), loan.getUuid(), loan.getAmount(), loan.getName());
+			return loan;
+		};
+	}
 }
